cd4330acd76fedd714db718ed14625f576437f2d
jmpark3972 <75054819+jmpark3972@users.noreply.github.com>
2025-07-31 02:42:48 +0900

    asdfsdf

--- nir/nir.py
+++ nir/nir.py
@@ -32,7 +32,7 @@ def init_nir():
     ads.data_rate = 128  # 128 SPS로 설정 (노이즈 감소)
     
     # 차동 입력과 싱글 입력 설정
-    ain_ir = AnalogIn(ads, ADS.P0, ADS.P2)  # 차동 (Vout‒1.65 V)
+    ain_ir = AnalogIn(ads, ADS.P0)  # 차동 (Vout‒1.65 V)
     ain_rtd = AnalogIn(ads, ADS.P1)  # 싱글 (RTD 노드)
     
     return i2c, ads, ain_ir, ain_rtd
