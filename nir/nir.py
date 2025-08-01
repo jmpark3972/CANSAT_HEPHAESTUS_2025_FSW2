ca91c83c336af27857824d9cb0e157bbab8759e0
jmpark3972 <75054819+jmpark3972@users.noreply.github.com>
2025-08-02 00:12:23 +0900

    asdfasdf

--- nir/nir.py
+++ nir/nir.py
@@ -13,7 +13,7 @@ ALPHA_NI = 0.006178  # 6178 ppm/K
 SENS_IR = 0.0034   # [V/°C] - 실측해 맞춘 감도
 
 # NIR 센서 설정
-NIR_OFFSET = 160.0  # 보정값 (V) - 손/책상 온도 보정
+NIR_OFFSET = 25  # 보정값 (V) - 손/책상 온도 보정
 NIR_SENSITIVITY = 1  # 감도: 전압 → 온도 변환 계수 (100.0 = 1V당 100°C)
 
 LOG_DIR = "sensorlogs"
@@ -60,8 +60,8 @@ def read_nir_with_calibration(chan0, chan1):
         v_tp = read_nir(chan0, chan1)  # 열전소자 전압
         v_rtd = chan1.voltage  # RTD 노드 전압
         
-        # 새로운 보정식 사용
-        t_obj = NIR_SENSITIVITY * (v_tp-v_rtd)*R_REF + NIR_OFFSET
+        # 새로운 보정식 사용asd
+        t_obj = NIR_SENSITIVITY * (v_tp-V_IN)*R_REF + NIR_OFFSET
         
         return v_rtd, t_obj
     except Exception as e:
