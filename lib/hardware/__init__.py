5156a87fd186f068ee1ffd3566b1ec3d08032a11
jmpark3972 <75054819+jmpark3972@users.noreply.github.com>
2025-08-07 06:06:36 +0900

    asdf

--- lib/hardware/__init__.py
+++ lib/hardware/__init__.py
@@ -4,7 +4,7 @@ CANSAT HEPHAESTUS 2025 FSW2 - 하드웨어 패키지
 하드웨어 관련 기능을 제공합니다.
 """
 
-from .i2c_manager import I2CManager, get_i2c_manager
+from .i2c_manager import I2CBusManager as I2CManager, get_i2c_manager
 from .force_kill import force_kill_process, force_kill_all_processes
 from .exception_handler import setup_exception_handler, handle_exception
 
