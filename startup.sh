5c71991c880db71e4fd00fda51ebb9527d79a64f
jmpark3972 <75054819+jmpark3972@users.noreply.github.com>
2025-08-07 07:15:17 +0900

    Refactor startup.sh to correct paths for python and virtual environment, enhance error messaging, and streamline activation process.

--- startup.sh
+++ startup.sh
@@ -1,22 +1,18 @@
-#!/bin/bash
 # This is the startup script
 # This script should be executed on startup
 # configure the path to contain the python code you want to run first
 # the path should be absolute
 
-# Example) python_path = /home/hyunlee/Desktop/flight_code/Python_Cansat_FSW
-
-python_path="/home/SpaceY/Desktop/CANSAT_HEPHAESTUS_2025_FSW2"
-venv_path="/home/SpaceY/Desktop/env"
+python_path="home/SpaceY/Desktop/CANSAT_HEPHAESTUS_2025_FSW2"
+venv_path="home/SpaceY/Desktop/env/bin"
 
 if [ "${python_path}" == "not_configured" ]; then
-    echo "Startup Script is not Configured! Please Edit the file"
+echo "Startup Script is not Configured! Please Edit the file"
+
 else
-    echo "Path > ${python_path}"
-    echo "venv > ${venv_path}/bin/activate"
-    
-    # Activate virtual environment and run main.py
-    source ${venv_path}/bin/activate
-    cd ${python_path}
-    python3 main.py
+echo "Path > ${python_path}"
+echo "venv > ${venv_path}/activate"
+. ${venv_path}/activate;
+cd ${python_path};python3 main.py
+
 fi
